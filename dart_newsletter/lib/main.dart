4a73b5964f38fdcacbf7ecbac64fa0a1da8a11fb
Yeikel Uriarte Arteaga <uyeikel@gmail.com>
2022-06-01 18:22:10 -0400

    feat(dart_newsletter): add dart example for form

---
+++ dart_newsletter/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:newsletter_form/newsletter_form.dart';
+
+void main(List<String> args) async {
+  final form = NewsletterForm();
+
+  print(form.state); // FormPureState()
+
+  form.email.dirty('laura@gmail');
+  await Future.delayed(Duration.zero);
+  print(form.state); // FormInvalidState()
+
+  form.email.dirty('laura@gmail.com');
+  await Future.delayed(Duration.zero);
+  print(form.state); // FormValidState()
+
+  form.submit();
+  await Future.delayed(const Duration(seconds: 4));
+  print(form.state); // FormSuccessState(You have successfully subscribed!)
+
+  form.close(); // Don't forget to close your bloc.
+}
