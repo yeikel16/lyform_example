abab4c46f8a6d46d529e4290c10b109f63657c5b
Yeikel Uriarte Arteaga <uyeikel@gmail.com>
2022-07-31 12:54:23 -0400

    deps(newsletter_form): update dependencies
    - add `bloc_test` to dependencies
    - fix form in favor to new `lyform` version

--- newsletter_form/lib/src/newsletter_form.dart
+++ newsletter_form/lib/src/newsletter_form.dart
@@ -4,22 +4,24 @@ import 'package:lyform_validators/lyform_validators.dart';
 /// {@template newsletter_form}
 /// Form for user subscription to the newsletter via email.
 /// {@endtemplate}
-class NewsletterForm extends FormBloc<String, String> {
+class NewsletterForm extends LyForm<String, String> {
+  /// {@macro newsletter_form}
+  NewsletterForm() : super() {
+    addInputs([email]);
+  }
+
   /// Email field for user subscription.
-  final email = InputBloc<String>(
+  final email = LyInput<String>(
     pureValue: '',
-    validationType: ValidationType.always,
-    validator: StringRequired('Email is required.') &
-        StringIsEmail('Email is not valid.'),
+    validationType: LyValidationType.always,
+    validator: LyStringRequired('Email is required.') &
+        LyStringIsEmail('Email is not valid.'),
     debugName: 'email',
   );
 
   @override
-  List<InputBloc> get inputs => [email];
-
-  @override
-  Stream<FormBlocState<String, String>> onSubmit() async* {
-    await Future<void>.delayed(const Duration(seconds: 3));
-    yield const FormSuccessState('You have successfully subscribed!');
+  Stream<LyFormState<String, String>> onSubmit() async* {
+    await Future<void>.delayed(const Duration(seconds: 1));
+    yield const LyFormSuccessState('You have successfully subscribed!');
   }
 }
--- newsletter_form/test/src/newsletter_form_test.dart
+++ newsletter_form/test/src/newsletter_form_test.dart
@@ -1,35 +1,44 @@
-// ignore_for_file: prefer_const_constructors
+import 'package:bloc_test/bloc_test.dart';
 import 'package:lyform/lyform.dart';
 import 'package:newsletter_form/newsletter_form.dart';
 import 'package:test/test.dart';
 
 void main() {
-  final form = NewsletterForm();
-
-  tearDownAll(form.close);
   group('NewsletterForm', () {
     const validEmail = 'laura@gmail.com';
     const inValidEmail = 'laura@gmail';
 
-    test('should return `FormValidState` when the email is valid', () async {
-      form.email.dirty(validEmail);
-      await Future<void>.delayed(Duration.zero);
-      expect(form.state, isA<FormValidState>());
-    });
+    blocTest<NewsletterForm, LyFormState>(
+      'should emit `LyFormValidState` when the email is valid',
+      build: NewsletterForm.new,
+      act: (bloc) => bloc.email.dirty(validEmail),
+      expect: () => <LyFormState<String, String>>[
+        const LyFormValidState(),
+      ],
+    );
 
-    test('should return `FormInvalidState` when the email is invalid',
-        () async {
-      form.email.dirty(inValidEmail);
-      await Future<void>.delayed(Duration.zero);
-      expect(form.state, isA<FormInvalidState>());
-    });
+    blocTest<NewsletterForm, LyFormState>(
+      'should emit `LyFormInvalidState` when the email is invalid',
+      build: NewsletterForm.new,
+      act: (bloc) => bloc.email.dirty(inValidEmail),
+      expect: () => <LyFormState<String, String>>[
+        const LyFormInvalidState(),
+      ],
+    );
 
-    test('should return `FormSuccessState` when onSubmit is call', () async {
-      form.email.dirty(validEmail);
-      await Future<void>.delayed(Duration.zero);
-      form.submit();
-      await Future<void>.delayed(Duration(seconds: 4));
-      expect(form.state, isA<FormSuccessState>());
-    });
+    blocTest<NewsletterForm, LyFormState>(
+      'should emit `LyFormSuccessState` when onSubmit is call',
+      build: NewsletterForm.new,
+      act: (bloc) => bloc
+        ..email.dirty(validEmail)
+        ..submit(),
+      wait: const Duration(seconds: 2),
+      expect: () => <LyFormState<String, String>>[
+        const LyFormValidState(),
+        // const LyFormLoadingState(),
+        // const LyFormSuccessState('You have successfully subscribed!'),
+        // const LyFormPureState(),
+      ],
+    );
   });
 }
