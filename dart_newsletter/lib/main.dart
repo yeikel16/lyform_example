0eddbfa549621f4e92b51796240e033752760147
Yeikel Uriarte Arteaga <uyeikel@gmail.com>
2022-08-01 11:44:47 -0400

    perf(dart_newsletter): update dart example

--- dart_newsletter/lib/main.dart
+++ dart_newsletter/lib/main.dart
@@ -3,19 +3,19 @@ import 'package:newsletter_form/newsletter_form.dart';
 void main(List<String> args) async {
   final form = NewsletterForm();
 
-  print(form.state); // FormPureState()
+  print(form.state); // LyFormPureState()
 
   form.email.dirty('laura@gmail');
   await Future.delayed(Duration.zero);
-  print(form.state); // FormInvalidState()
+  print(form.state); // LyFormInvalidState()
 
   form.email.dirty('laura@gmail.com');
   await Future.delayed(Duration.zero);
-  print(form.state); // FormValidState()
+  print(form.state); // LyFormValidState()
 
   form.submit();
-  await Future.delayed(const Duration(seconds: 4));
-  print(form.state); // FormSuccessState(You have successfully subscribed!)
+  await Future.delayed(const Duration(seconds: 2));
+  print(form.state); // LyFormSuccessState(You have successfully subscribed!)
 
   form.close(); // Don't forget to close your bloc.
 }
